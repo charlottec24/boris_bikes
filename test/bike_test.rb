ee2399e80e161b45bf1445a2a4268ff8173a06ef
Charlotte Cameron <charlottemidgley@gmail.com>
2013-05-31 09:39:46 +0100

    Amended bike_test.rb - made shorter.

--- test/bike_test.rb
+++ test/bike_test.rb
@@ -3,25 +3,26 @@ require '../lib/bike'
 
 class BikeTest < MiniTest::Test
 
+	def setup
+		@bike = Bike.new(1)
+	end
+
 	def test_new_bike_is_not_broken
-		bike = Bike.new(1)
-		assert bike.broken? == false
+		assert @bike.broken? == false
 	end
 
 	def test_bike_can_be_broken
-		bike = Bike.new
-		bike.break!
-		assert bike.broken? == true
+		@bike.break!
+		assert @bike.broken? == true
 		10.times do |i|
 			Bike.new(i)
 		end
 	end
 
 	def test_bike_can_be_fixed
-		bike = Bike.new
-		bike.break!
-		bike.fix!
-		assert bike.broken? == false
+		@bike.break!
+		@bike.fix!
+		assert @bike.broken? == false
 	end
 
 end
